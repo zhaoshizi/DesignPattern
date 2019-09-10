d6779a6af4c9a375b27cc2b7cf6f59c5db5a61d1
ZS <zhaoshizi-zs@163.com>
2019-09-11 00:08:15 +0800

    分割回文串

--- JavaPractice/src/main/java/leetcodepractice/splitpalindrome/Solution.java
+++ JavaPractice/src/main/java/leetcodepractice/splitpalindrome/Solution.java
@@ -1,6 +1,7 @@
 package leetcodepractice.splitpalindrome;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
@@ -12,39 +13,47 @@ class Solution {
         for(int i = 0; i<=s.length(); i++){
 
             for(int j = i+1; j<=s.length(); j++){
-                if(palindromeFlag[0][i] == true && isPalindrome(s.substring(i, j))){
+                if(palindromeFlag[0][i] != null && isPalindrome(s.substring(i, j))){
                     palindromeFlag[i+1][j] = true;
                     palindromeFlag[0][j] = true;
                 }
             }
         }
 
-        for(int i=0; i<=s.length();i++)
-            for(int j=0;j<=s.length();j++){
-                System.out.print(palindromeFlag[i][j] + " ");
-                if(j == s.length())
-                    System.out.println();
-            }
+        // for(int i=0; i<=s.length();i++)
+        //     for(int j=0;j<=s.length();j++){
+        //         System.out.print(palindromeFlag[i][j] + " ");
+        //         if(j == s.length())
+        //             System.out.println();
+        //     }
         
         List<List<String>> llstr= new ArrayList<List<String>>();
         List<String> lstr = new ArrayList<String>();
 
-        for(int i =1;i<=s.length();i++){
-            for(int j =i;j<=s.length();j++){
+        makeString(s,1,1,palindromeFlag,lstr,"");
 
-            }
+        for (String str : lstr) {
+            //System.out.println(str);
+            llstr.add(new ArrayList<>(Arrays.asList(str.split(" "))));
         }
 
-        return new  ArrayList<List<String>>() ;
+        // for(List<String> lstr1:llstr)
+        //     System.out.println(lstr1);
+
+        return llstr;
     }
 
-    public static  void makeString(String s,int b,int e,Boolean[][] palindromeFlag,List<List<String>> llstr,List<String> lstr ){
-        for(int i=0;e+i<=s.length();i++){
-            if(palindromeFlag[b][e] == true ){
-                lstr.add(s.substring(b-1,e));
+    public static  void makeString(String s,int b,int e,Boolean[][] palindromeFlag,List<String> lstr,String str ){
+        if(b>s.length()){
+            lstr.add(str);
+            return;
+        }
+            
+        for(int i=e;i<=s.length();i++){
+            if(palindromeFlag[b][i] != null ){
+                makeString(s,i+1,i+1,palindromeFlag,lstr,(str=="")?s.substring(b-1,i):str + " " + s.substring(b-1,i));
             }
         }
-        
 
     }
 
@@ -73,9 +82,23 @@ class Solution {
         return false;
     }
 
+    private boolean isPalindrome1(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        String str = "aab";
-        partition(str);
+        String str = "aa";
+        List<List<String>> llstr = partition(str);
+        for(List<String> lstr1:llstr)
+             System.out.println(lstr1);
 
     }
 
