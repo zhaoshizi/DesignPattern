8360876d87b6b839c0cc0dfb74f3c0bb448a2781
zhaoshizi <zhaoshizi-zs@163.com>
2019-10-10 20:17:37 +0800

    程序结构进行了优化

--- JavaPractice/src/main/java/leetcodepractice/searchwords/Solution.java
+++ JavaPractice/src/main/java/leetcodepractice/searchwords/Solution.java
@@ -62,41 +62,34 @@ public class Solution {
     }
 
     public List<String> findWords(char[][] board, String[] words) {
-        int n = board.length;
-        int m = board[0].length;
-
         Node headNode = makeTrie(words);
         List<String> strList = new ArrayList<String>();
         String str = "";
         //遍历二维数组，查找一个字符串的起点
-        for(int i =0 ;i<n;i++)
-            for(int j =0; j<m;j++){
+        for(int i =0 ;i<board.length;i++)
+            for(int j =0; j<board[0].length;j++){
                 if(headNode.nodeList[board[i][j] -'a'] != null ){
-                    char c = board[i][j];
-                    //特殊处理，把字符变为大写，避免在同一个遍历过程中重复遍历
-                    board[i][j] = Character.toUpperCase(board[i][j]);
-                    //开始遍历，并判断是否可以剪枝
-                    if(findchar(strList,str+c,board,headNode.nodeList[c-'a'],i,j)){
-                        headNode.nodeList[c-'a'] =null;
-                        //若剪权，则减少分支
-                        headNode.num--;
-                        //分支减为零，则直接返回
-                        if(headNode.num ==0)
-                            return strList;
+                    //开始遍历，并判断是否还有分支
+                    if(findchar(strList,str,board,headNode,i,j)){
+                        //没有分支直接返回
+                        return strList;
                     }
-                    //还原来还字符
-                    board[i][j] = c ;
                 }
             }
 
         return strList;
     }
     //一个遍历过程，返回值表示该节点是否可以剪枝，返回true为没有分支，可以剪枝
-    public boolean findchar(List<String> strList,String str,char[][]board,Node p,int i,int j){
+    public boolean findchar(List<String> strList,String str,char[][]board,Node q,int i,int j){
+        //开始遍历对应的分支
+        Node p = q.nodeList[board[i][j]-'a'];
+        //记录遍历的节点，特殊处理，以避免重复遍历
+        char c = board[i][j];
+        board[i][j] = '@';
         //判断该节点是否为一个字符串的最后一个字符
         if(p.isLeaf == true){
             //如果是最后一个字符，说明查到一个字符串，加到列表里
-            strList.add(str);
+            strList.add(str+p.ch);
             //修改是否是最后一个字符的标记，避免重复查到同一个字符串
             //以后遍历时，即便同到的路径找到该节点，也不会识别为一个字符串
             p.isLeaf = false;
@@ -104,58 +97,44 @@ public class Solution {
         //遍历该二维数组元素上下左右四个元素
         for(int k =-1 ;k<=1;k=k+2){
             if(i+k>=0 && i+k<board.length){
-                //先判断该元素不是大写字母，因为大写字母是遍历过的
-                if(board[i+k][j]>='a' && p.nodeList[board[i+k][j]-'a'] != null){
-                    char c = board[i+k][j];
-                    board[i+k][j] = Character.toUpperCase(board[i+k][j]);
-                    if(findchar(strList,str+c,board,p.nodeList[c-'a'],i+k,j)){
-                        p.nodeList[c-'a'] = null;
-                        p.num--;
-                        if(p.num==0){
-                            board[i+k][j] = c;
-                            //没有分支，可以把该节点从前缀树中去掉
-                            return true;
-                        }
-                    } 
-                    board[i+k][j] = c;
-                }  
+                //开始遍历，并判断是否还有分支
+                if(board[i+k][j] !='@' && p.num > 0 && p.nodeList[board[i+k][j]-'a'] != null && findchar(strList,str+p.ch,board,p,i+k,j))
+                    //没有分支就直接返回
+                    break;
             }
             if(j+k>=0 && j+k<board[0].length){
-                //先判断该元素不是大写字母，因为大写字母是遍历过的
-                if(board[i][j+k]>='a' && p.nodeList[board[i][j+k]-'a'] != null){
-                    char c = board[i][j+k];
-                    board[i][j+k] = Character.toUpperCase(board[i][j+k]);
-                    if(findchar(strList,str+c,board,p.nodeList[c-'a'],i,j+k)){
-                        p.nodeList[c-'a'] = null;
-                        p.num--;
-                        if(p.num==0){
-                            board[i][j+k] = c;
-                            //没有分支，可以把该节点从前缀树中去掉
-                            return true;
-                        }
-                    }   
-                    board[i][j+k] = c;
-                }
+                //开始遍历，并判断是否还有分支
+                if(board[i][j+k] !='@' && p.num > 0 && p.nodeList[board[i][j+k]-'a'] != null && findchar(strList,str+p.ch,board,p,i,j+k))
+                    //没有分支就直接返回
+                    break;
             }    
         }
+        //恢复该节点的值
+        board[i][j] = c;
         //还有分支表示不能剪枝
         if (p.num>0)
             return false;
-        return true;
+        else{
+            //p是q的子节点，p没有分支，可以在q里去掉p分支
+            q.nodeList[board[i][j]-'a'] = null;
+            q.num--;
+        }
+        //返回q是否还有分支
+        return q.num==0;
     }
 
     public static void main(String[] args) {
-        String[] words = {"oath","pea","eat","rain"};
-        char[][] board ={
-                        {'o','a','a','n'},
-                        {'e','t','a','e'},
-                        {'i','h','k','r'},
-                        {'i','f','l','v'}
-                        };
+        // String[] words = {"oath","pea","eat","rain"};
+        // char[][] board ={
+        //                 {'o','a','a','n'},
+        //                 {'e','t','a','e'},
+        //                 {'i','h','k','r'},
+        //                 {'i','f','l','v'}
+        //                 };
         // String[] words = {"aaa"};
         // char[][] board ={{'a','a'}};
-        // String[] words = {"aba","baa","bab","aaab","aaa","aaaa","aaba"};
-        // char[][] board ={{'a','b'},{'a','a'}};
+        String[] words = {"aba","baa","bab","aaab","aaa","aaaa","aaba"};
+        char[][] board ={{'a','b'},{'a','a'}};
         List<String> strList= new Solution().findWords( board, words);
         for(String str:strList){
             System.out.println(str);
